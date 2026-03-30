c014df13ced1cffe20ec90ddfdf05c5f545ae4cd
tanvi harde <devikah369@gmail.com>
2026-03-30 14:08:04 +0530

    done

--- lib/src/interface/validators.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_validation/src/validation_support/email_validation_support.dart';
-
-abstract class Validators {
-  EmailValidationSupport? emailValidator({required String? value});
-
-  String? contactValidator({required String? value});
-}
