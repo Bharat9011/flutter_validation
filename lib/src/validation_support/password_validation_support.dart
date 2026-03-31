c920d658f88489796c22821b342519c808d6c2b2
tanvi harde <devikah369@gmail.com>
2026-03-31 11:13:54 +0530

    updated and added new files

--- lib/src/validation_support/password_validation_support.dart
+++ lib/src/validation_support/password_validation_support.dart
@@ -4,63 +4,66 @@ class PasswordValidationSupport {
 
   PasswordValidationSupport(String value) : _value = value;
 
-  PasswordValidationSupport isRequired() {
+  PasswordValidationSupport isRequired({String? message}) {
     if (_value.trim().isEmpty) {
-      _error ??= "Password is required";
+      _error ??= message ??"Password is required";
     }
     return this;
   }
 
-  PasswordValidationSupport minLength(int length) {
+  PasswordValidationSupport minLength(int length, {String? message}) {
     if (_value.length < length) {
-      _error ??= "Password must be at least $length characters";
+      _error ??= message ??  "Password must be at least $length characters";
     }
     return this;
   }
 
-  PasswordValidationSupport maxLength(int length) {
+  PasswordValidationSupport maxLength(int length, {String? message}) {
     if (_value.length > length) {
-      _error ??= "Password must be less than $length characters";
+      _error ??= message ??  "Password must be less than $length characters";
     }
     return this;
   }
 
-  PasswordValidationSupport hasUppercase() {
+  PasswordValidationSupport hasUppercase({String? message}) {
     if (!RegExp(r'[A-Z]').hasMatch(_value)) {
-      _error ??= "Password must contain at least one uppercase letter";
+      _error ??= message ??  "Password must contain at least one uppercase letter";
     }
     return this;
   }
 
-  PasswordValidationSupport hasLowercase() {
+  PasswordValidationSupport hasLowercase({String? message}) {
     if (!RegExp(r'[a-z]').hasMatch(_value)) {
-      _error ??= "Password must contain at least one lowercase letter";
+      _error ??= message ?? "Password must contain at least one lowercase letter";
     }
     return this;
   }
 
-  PasswordValidationSupport hasNumber() {
+  PasswordValidationSupport hasNumber({String? message}) {
     if (!RegExp(r'[0-9]').hasMatch(_value)) {
-      _error ??= "Password must contain at least one number";
+      _error ??= message ?? "Password must contain at least one number";
     }
     return this;
   }
 
-  PasswordValidationSupport hasSpecialCharacter() {
+  PasswordValidationSupport hasSpecialCharacter({String? message}) {
     if (!RegExp(r'[!@#$%^&*(),.?":{}|<>_\-\\/\[\]=+;]').hasMatch(_value)) {
-      _error ??= "Password must contain at least one special character";
+      _error ??= message ?? "Password must contain at least one special character";
     }
     return this;
   }
 
-  PasswordValidationSupport noSpaces() {
+  PasswordValidationSupport noSpaces({String? message}) {
     if (_value.contains(' ')) {
-      _error ??= "Password should not contain spaces";
+      _error ??= message ??  "Password should not contain spaces";
     }
     return this;
   }
 
-  custom(bool Function(String value) validator, String message) {
+  PasswordValidationSupport custom(
+    bool Function(String value) validator,
+    String message,
+  ) {
     if (_error != null) return this;
 
     if (!validator(_value)) {
